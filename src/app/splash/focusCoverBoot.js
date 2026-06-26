92dbcca3c3571fa9d03b8b50564a153cfc28f56d
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-26 03:35:17 -0400

    Skip splash and observers inside rail iframes

--- src/app/splash/focusCoverBoot.js
+++ src/app/splash/focusCoverBoot.js
@@ -1,6 +1,11 @@
-import '../landing/portalLandingBoot.js';
+import { installPortalLandingObserver } from '../landing/portalLandingBoot.js';
 import { installPageFrameSurfaceObserver } from '../launcher/pageFrameSurface.js';
 import { installFocusCoverSplash } from './focusCoverSplash.js';
 
-installPageFrameSurfaceObserver();
-installFocusCoverSplash();
+const isEmbedded = window.self !== window.top || new URLSearchParams(window.location.search).has('embed');
+
+if (!isEmbedded) {
+  installPortalLandingObserver();
+  installPageFrameSurfaceObserver();
+  installFocusCoverSplash();
+}
